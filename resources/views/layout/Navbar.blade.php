95cfbf1eb8d968068389386708c8d71134fb87cc
ArmanNagariya-Developer <armannagariya@gmail.com>
2025-01-22 23:35:11 +0530

    Initial commit

--- resources/views/layout/Navbar.blade.php
+++ resources/views/layout/Navbar.blade.php
@@ -1,28 +1,46 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Home Page</title>
-        <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        <!-- Bootstrap CSS v5.2.1 -->
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
+        <link rel="stylesheet" href="./css">
     </head>
 
     <body>
         <header>
-            @extends('layout/Navbar')
+           <div class="container-fluid bg-dark text-light p-3">
+            <div class="row">
+                <div class="col-sm-4">
+                    <img src="assets/HealthEaseLogo.png" alt="" srcset="" class="img-logo">
+                </div>
+                <div class="col-auto ">
+                    Home
+                </div>
+                <div class="col-auto">
+                    Home
+                </div>  <div class="col-auto">
+                    Home
+                </div>  <div class="col-auto">
+                    Home
+                </div>
+            </div>
+           </div>
         </header>
-        <main></main>
+        <main>
+            <div class="container">
+                @yield('container')
+            </div>
+        </main>
         <footer>
             <!-- place footer here -->
         </footer>
